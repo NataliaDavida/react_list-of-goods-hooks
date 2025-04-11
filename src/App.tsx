0167ab84aa8703cf72df4ee3ea4b2f5b77956a5a
NataliaDavida <nataliadavida88@gmail.com>
2025-04-11 19:49:53 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,35 +16,73 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+function sortAlphabrtically(someList: string[], params: string) {
+  const preperedList = [...someList];
+
+  switch (params) {
+    case 'alpha':
+      return preperedList.sort((a, b) => a.localeCompare(b));
+    case 'length':
+      return preperedList.sort((a, b) => a.length - b.length);
+    case 'reverse':
+      return preperedList.reverse();
+    default:
+      return preperedList;
+  }
+}
+
 export const App: React.FC = () => {
+  const [filterParams, setFilterParams] = useState('');
+  const visiblegoods = sortAlphabrtically(goodsFromServer, filterParams);
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={() => setFilterParams('alpha')}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={() => setFilterParams('length')}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          data-cy="ReverseButton"
+          className={cn('button', 'is-warning', {
+            'is-light': filterParams !== 'reverse',
+          })}
+          onClick={() => setFilterParams('reverse')}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {filterParams !== '' && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setFilterParams('')}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
         <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
+          {visiblegoods.map(good => (
+            <li key={good} data-cy="Good">
+              {good}
+            </li>
+          ))}
         </ul>
       </ul>
     </div>
